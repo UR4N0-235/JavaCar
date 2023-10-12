ef900a60862c400aa60ea7300eeff0088a0bef7b
UR4N0-235 <matheus.fernandes92.235@protonmail.com>
2023-10-11 21:27:00 -0300

    first commit

---
+++ src/main/java/com/ur4n0/avaliacaobackendjava/AvaliacaobackendjavaApplication.java
@@ -0,0 +1,13 @@
+package com.ur4n0.avaliacaobackendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaobackendjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaobackendjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ur4n0/avaliacaobackendjava/AvaliacaobackendjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ur4n0.avaliacaobackendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaobackendjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
